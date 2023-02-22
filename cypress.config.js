8f800a2f71754708fe391416000314f805ad390d
IgnasR <racinskas.i@gmail.com>
2023-02-22 18:51:15 +0200

    cucumber setup, config adjustments

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,22 @@
 const { defineConfig } = require("cypress");
-
+const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
+const addCucumberPreprocessorPlugin = require("@badeball/cypress-cucumber-preprocessor").addCucumberPreprocessorPlugin;
+const createEsbuildPlugin = require("@badeball/cypress-cucumber-preprocessor/esbuild").createEsbuildPlugin;
 module.exports = defineConfig({
+  viewportWidth: 1920,
+  viewportHeight: 1080,
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    async setupNodeEvents(on, config) {
+      const bundler = createBundler({
+        plugins: [createEsbuildPlugin(config)],
+      });
+
+      on("file:preprocessor", bundler);
+      await addCucumberPreprocessorPlugin(on, config);
+
+      return config;
     },
+    specPattern: "cypress/e2e/scenarios/*.feature",
+    baseUrl: "https://spectrocoin.com/en/bitcoin-price-rates.html"
   },
 });
